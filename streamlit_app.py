814209c373e251f170eb7632518c574fac710c18
dhontana-bit <danihontana1999@gmail.com>
2025-09-18 11:29:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
@@ -19,26 +18,30 @@ cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCHON'))
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+# st.dataframe(pd_df)
+# st.stop()
 
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
 
-name=st.text_input("How is your name?")
-if name:
-    ingredients = st.multiselect(
-        "Hi "+name+", choose up 5 ingredients!",
-        my_dataframe,
-        max_selections=5
-        
-    )
-    if ingredients:     
-        ingredients_string=''
-    
-        for fruit_chosen in ingredients:
-            ingredients_string+= fruit_chosen +' '
-            st.subheader(fruit_chosen + ' Nutrition Information')
-            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-            sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         st.write(ingredients_string)
     
