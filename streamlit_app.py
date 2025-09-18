ea6e43ff75b13b9b71fb0ae0d650b0f2e12b7c55
dhontana-bit <danihontana1999@gmail.com>
2025-09-18 11:12:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,3 @@ if name:
         if time_to_insert:
             session.sql(my_insert_stmt).collect()
             st.success('Your Smoothie is ordered '+name+'!', icon="✅")
-
-
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-
