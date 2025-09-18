596f551d7261fb954c8411a5c64b69ce3d3e5801
dhontana-bit <danihontana1999@gmail.com>
2025-09-18 11:05:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if name:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
